bd77d1b7c64fb5f01efa2b8a74d9dbab85b9e343
emmyjaff <emmyjaff22@gmail.com>
2022-09-19 02:11:05 +0100

    first publish and instanciation of the application

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>We are about to begin work!!</Text>
+      <Text> Join the working team now!</Text>
       <StatusBar style="auto" />
     </View>
   );
